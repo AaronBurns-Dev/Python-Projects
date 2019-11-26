020aa21bd2779a222db7403d1feacc80db8fab67
AaronBurns-Dev <58194679+AaronBurns-Dev@users.noreply.github.com>
2019-11-25 19:25:21 -0600

    Delete pathTest.py
    Restructuring

--- pathTest.py
+++
@@ -1,9 +0,0 @@
-import os
-import os.path
-
-PATH = './deleteCookie.txt'
-
-if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-    print ("My roflcopter goes soi soi soi soi")
-else:
-    print ("Sadface :( ")
